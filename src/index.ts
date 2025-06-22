a9b432e9737cfbab00f8f31a4441f12e726f1dde
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-22 22:06:15 +0200

    setting up express

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import {json} from 'body-parser';
+//import autoRoute from 
+const app = express();
+app.use(json());
+
+//app.use('')
+app.get('/', (req, res) => {
+    console.log('test');
+  res.send('yes, it works!');
+});
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
