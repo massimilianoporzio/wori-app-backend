b35e52081cf79d18199bb2a74dd66bd675f98b9e
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-23 15:55:44 +0200

    prova da ufficio

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import {json} from 'body-parser';
-//import autoRoute from 
+import express from "express";
+import { json } from "body-parser";
+//import autoRoute from
 const app = express();
 app.use(json());
 
 //app.use('')
-app.get('/', (req, res) => {
-    console.log('test');
-  res.send('yes, it works!');
+app.get("/", (req, res) => {
+  console.log("test");
+  res.send("yes, it works (anche da ufficio)!");
 });
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
