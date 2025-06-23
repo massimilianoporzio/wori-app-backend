21b145d3b50e7dfe86c9e461b7caca3fbef0078f
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-06-23 22:31:50 +0200

    rimesso porta std

--- src/models/supaDB.ts
+++ src/models/supaDB.ts
@@ -1,6 +1,6 @@
 import postgres from 'postgres'
 console.log('Connecting to PostgreSQL database...', process.env.DATABASE_URL )
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:Ma$$ichiara07@localhost:5433/woridb'
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:Ma$$ichiara07@localhost:5432/woridb'
 console.log('Using connection string:', connectionString)
 const sql = postgres(connectionString) // will use psql environment variables
 
